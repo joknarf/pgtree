91038d80c4f6ddafefbdeecf1eca47e446039979
joknarf <joknarf@free.fr>
2023-01-31 18:49:41 +0100

    version 1.0.26

--- pgtree/_version.py
+++ pgtree/_version.py
@@ -7,5 +7,5 @@ Provides pgtree version information.
 
 from incremental import Version
 
-__version__ = Version("pgtree", 1, 0, 25)
+__version__ = Version("pgtree", 1, 0, 26)
 __all__ = ["__version__"]
